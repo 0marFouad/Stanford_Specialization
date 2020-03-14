d53b1aba480222ee8db656ec938ca2a13cce5d4f
OmarFouad <omar.ahmed.fouad1996@gmail.com>
2020-03-14 02:50:06 +0200

    Fixing Empty Commit

--- src/Course_2/StreamingMedians/Driver.java
+++ src/Course_2/StreamingMedians/Driver.java
@@ -1,4 +1,39 @@
 package Course_2.StreamingMedians;
 
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Driver {
+    public static void main(String[] args){
+        ArrayList<Integer> data = readData();
+        long answer = 0;
+        Median_Maintenance solver = new Median_Maintenance();
+        for(int i=0;i<data.size();i++){
+            solver.addNumber(data.get(i));
+            answer += solver.getMedian()%10000;
+            answer %= 10000;
+        }
+        System.out.println(answer);
+    }
+
+
+    public static ArrayList<Integer> readData(){
+        ArrayList<Integer> data = new ArrayList<>();
+        File file = new File("/home/fo2sh/Desktop/Educational/Algorithms/Stanford_Assignments/src/Course_2/StreamingMedians/medians.txt");
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String st;
+            while ((st = br.readLine()) != null){
+                int from = Integer.parseInt(st);
+                data.add(from);
+            }
+        }catch (IOException e){
+            System.out.println(e);
+        }
+        return data;
+    }
 }
--- src/Course_2/StreamingMedians/Median_Maintenance.java
+++ src/Course_2/StreamingMedians/Median_Maintenance.java
@@ -1,4 +1,52 @@
 package Course_2.StreamingMedians;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
 public class Median_Maintenance {
+    PriorityQueue<Integer> maxPQ;
+    PriorityQueue<Integer> minPQ;
+    int size;
+
+    Median_Maintenance(){
+        maxPQ = new PriorityQueue<>(Collections.reverseOrder());
+        minPQ = new PriorityQueue<>();
+        size = 0;
+    }
+
+    public void addNumber(int num){
+        if(size==0){
+            maxPQ.add(num);
+        }else if(size == 1){
+            int cur = maxPQ.isEmpty() ? minPQ.poll() : maxPQ.poll();
+            int small = Math.min(cur, num);
+            int large = Math.max(cur, num);
+            minPQ.add(large);
+            maxPQ.add(small);
+        }else{
+            if(num > minPQ.peek()){
+                minPQ.add(num);
+            }else if(num < maxPQ.peek()){
+                maxPQ.add(num);
+            }else{
+                maxPQ.add(num);
+            }
+        }
+        while(minPQ.size() > maxPQ.size()){
+            maxPQ.add(minPQ.poll());
+        }
+        while(maxPQ.size() > minPQ.size() + 1){
+            minPQ.add(maxPQ.poll());
+        }
+        size++;
+    }
+
+    public int getMedian(){
+        return maxPQ.peek();
+    }
 }
